26424bdf5ada4e9636146f73501620f7fb7d64b8
Tms <332477694@qq.com>
2016-12-08 17:30:58 +0800

    Init

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Contracts\View\Factory as ViewFactory;
+
+/**
+ * Get the evaluated view contents for the given view.
+ *
+ * @param  string  $view
+ * @param  array   $data
+ * @param  array   $mergeData
+ * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
+ */
+function themeView($view = null, $data = [], $mergeData = [])
+{
+    $factory = app(ViewFactory::class);
+
+    if (func_num_args() === 0) {
+        return $factory;
+    }
+
+    return $factory->make($view, $data, $mergeData);
+}
