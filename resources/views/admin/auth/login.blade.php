37c6bf6f2e65105e64d6636f77ad070135d34fea
Tms <332477694@qq.com>
2016-12-08 17:58:46 +0800

    Init

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -7,11 +7,23 @@
 
         <title>Laravel</title>
 
-
+        <script src="{{asset("/common/js/jquery-1.12.3.min.js")}}"></script>
+        <script src="{{asset("/common/js/bootstrap.min.js")}}"></script>
+        <link href="{{asset("/common/css/bootstrap.min.css")}}" rel="stylesheet" />
 
     </head>
     <body>
         <div class="container-fluid">
+            <div class="col-md-4 col-md-offset-4">
+                <form action="" class="form-horizontal">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <div class="input-group-addon"><span class="glyphicon glyphicon-user"></span></div>
+                            <input class="form-control" type="email" placeholder="Enter email">
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </body>
 </html>
